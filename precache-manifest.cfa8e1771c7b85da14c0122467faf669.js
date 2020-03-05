a0664852fbcfd6db961e49546b289f7c06de964f
Karan Thakkar <karan.d.thakkar@gmail.com>
2020-03-05 15:26:35 -0500

    Updates

---
+++ precache-manifest.cfa8e1771c7b85da14c0122467faf669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28b7873e6ffbc9f75dc5dba6350909e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d4467d255d3a3125eb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9b0781cc51fac59feaa",
+    "url": "/static/js/2.a2d0dce5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a2d0dce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d4467d255d3a3125eb",
+    "url": "/static/js/main.e674aae2.chunk.js"
+  },
+  {
+    "revision": "e6dd1f3f5dc377bad9e2",
+    "url": "/static/js/runtime-main.35cd3e42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
