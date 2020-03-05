28742db01c21255b3665a4714dc10954d9738b02
Karan Thakkar <karan.d.thakkar@gmail.com>
2020-03-05 17:34:42 -0500

    under maintenance

---
+++ src/Components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Header, Icon, Grid } from 'semantic-ui-react';
+import './App.css';
+
+
+function App() {
+  return (
+    <Grid style={{ height: '100vh', backgroundColor:'#000000' }}>
+      <Grid.Row>
+        <Grid.Column textAlign='center' verticalAlign='middle'>
+          <Icon loading inverted name='spinner' size='massive' />
+          <Header as='h2' inverted content='Site Under Maintenance' />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default App;
